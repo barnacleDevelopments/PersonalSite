4cd9cf05ef029499ae08ae8ce6da5fc9bc8ffa5a
Devin Davis <devin@warriertech.com>
2024-03-23 13:04:26 -0300

    (styling) mobile styling for progress gauge on projects page

--- src/components/projects/ProgressGauge.js
+++ src/components/projects/ProgressGauge.js
@@ -57,37 +57,43 @@ const ProgressGauge = ({ maxProgress = 100, currentProgress = 0 }) => {
             transition: "width 0.5s ease-out",
           }}
         ></Box>
-        {[...Array(6)].map((x, i) => {
-          const segment = `${((maxProgress / 5) * i).toFixed(4)} eth`;
+        <Box
+          sx={{
+            display: ["none", "block"],
+          }}
+        >
+          {[...Array(6)].map((x, i) => {
+            const segment = `${((maxProgress / 5) * i).toFixed(4)} eth`;
 
-          return (
-            <React.Fragment key={i}>
-              {i != 0 && i != 5 && (
+            return (
+              <React.Fragment key={i}>
+                {i != 0 && i != 5 && (
+                  <Box
+                    sx={{
+                      position: "absolute",
+                      left: `${(i * 100) / 5}%`,
+                      top: 0,
+                      bottom: 0,
+                      borderLeft: "2px solid white",
+                    }}
+                  ></Box>
+                )}
                 <Box
                   sx={{
                     position: "absolute",
                     left: `${(i * 100) / 5}%`,
-                    top: 0,
-                    bottom: 0,
-                    borderLeft: "2px solid white",
+                    top: "25px",
+                    fontSize: "10px",
+                    transform: "translateX(-50%)",
+                    whiteSpace: "nowrap",
                   }}
-                ></Box>
-              )}
-              <Box
-                sx={{
-                  position: "absolute",
-                  left: `${(i * 100) / 5}%`,
-                  top: "25px",
-                  fontSize: "10px",
-                  transform: "translateX(-50%)",
-                  whiteSpace: "nowrap",
-                }}
-              >
-                {segment}
-              </Box>
-            </React.Fragment>
-          );
-        })}
+                >
+                  {segment}
+                </Box>
+              </React.Fragment>
+            );
+          })}
+        </Box>
       </Box>
     </Box>
   );
