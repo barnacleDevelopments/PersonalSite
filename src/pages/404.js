da18241f20e2f955f456251190a5005f6fc601d3
Devin Davis <devin@warriertech.com>
2024-06-22 22:24:00 -0400

    (feat) go back home button on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 // markup
 const NotFoundPage = () => {
   return (
     <main>
       <h1>Oups! This page does not exist!</h1>
+      <Link href="/">Go Back Home</Link>
     </main>
   )
 }
