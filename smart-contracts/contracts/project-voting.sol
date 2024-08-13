16aad3fd971e21f84fda3e809f5e94abb26565be
Devin Davis <devin@warriertech.com>
2024-08-13 00:44:45 -0400

    update vrf number generator from 2.0 to 2.5

--- smart-contracts/contracts/project-voting.sol
+++ smart-contracts/contracts/project-voting.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/utils/Address.sol";
-import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract ProjectVoting is VRFConsumerBaseV2{
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {VRFV2PlusWrapperConsumerBase} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFV2PlusWrapperConsumerBase.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
+contract ProjectVoting is VRFConsumerBaseV2Plus {
     // Voting mappings
     mapping(uint => mapping(string => uint)) public cycleVotes; // total project votes within the current cycle
     mapping(uint => mapping(address => string)) public addressCycleChoice; // address project choice for the currrent cycle
@@ -15,13 +20,11 @@ contract ProjectVoting is VRFConsumerBaseV2{
     mapping(address => string) public addressNames; // address display names
     mapping(string => string) public projects;
 
-    address private owner;
     uint private threshold = 0.1 ether;
-    uint public currentCycle = 1; // <= current cycle
+    uint public currentCycle = 1;
 
     // Chainlink VRF Variables
-    VRFCoordinatorV2Interface COORDINATOR;
-    uint64 private subscriptionId; // this can be found here https://vrf.chain.link/sepolia
+    uint256 s_subscriptionId;
     bytes32 private keyHash;
     uint32 private callbackGasLimit = 100000; // Adjust the gas limit based on the requirement
     uint16 private requestConfirmations = 3; // Minimum number of confirmations
@@ -29,30 +32,33 @@ contract ProjectVoting is VRFConsumerBaseV2{
 
     // Voting Events
     event ProjectAdded(string projectId, string projectName);
-    event Voted(address voter, string name, string projectId); 
-    event WinnerAnnounced(address winner, uint256 amount);
+    event Voted(address voter, string name, string projectId, uint cycle);
+    event WinnerAnnounced(address winner, uint256 amount, uint cycle);
 
     // User Action Events
     event ActionPerformed(address voter, string[] actionCIDs);
 
     // Chainlink VRF Events
-    event RandomNumberRequested(uint256 requestId);
-    event RandomNumberReceived(uint256 randomNumber);
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can execute");
-        _;
+    // Chainlink VRF Past Request IDs.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
     }
-  
-    constructor(
-        address _vrfCoordinator,
-        uint64 _subscriptionId,
-        bytes32 _keyHash
-    ) VRFConsumerBaseV2(_vrfCoordinator) {
-        owner = msg.sender;
-        subscriptionId = _subscriptionId;
+
+    mapping(uint256 => RequestStatus)
+
+    public s_requests;
+
+    constructor(address _vrfCoordinator, uint256 _subscriptionId, bytes32 _keyHash) VRFConsumerBaseV2Plus(_vrfCoordinator) {
+        s_subscriptionId = _subscriptionId;
         keyHash = _keyHash;
-        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
     }
 
     function add(string memory projectName, string memory projectId) public onlyOwner {
@@ -73,8 +79,8 @@ contract ProjectVoting is VRFConsumerBaseV2{
         cycleVotes[currentCycle][projectId]++;
         hasVotedForCycle[currentCycle][msg.sender] = true;
         cycleVoters[currentCycle].push(msg.sender);
-        // checkAndTransfer();
-        emit Voted(msg.sender, voterName, projectId);
+        checkAndTransfer();
+        emit Voted(msg.sender, voterName, projectId, currentCycle);
     }
 
     function getCycleVoteCount(string memory projectId) public view returns (uint) {
@@ -97,37 +103,61 @@ contract ProjectVoting is VRFConsumerBaseV2{
         return threshold;
     }
 
+    function requestRandomWords(
+        bool enableNativePayment
+    ) internal onlyOwner returns (uint256 requestId) {
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({
+                        nativePayment: enableNativePayment
+                    })
+                )
+            })
+        );
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId;
+    }
+
     function checkAndTransfer() internal {
         if (address(this).balance >= threshold) {
             require(cycleVoters[currentCycle].length > 0, "No voters to reward");
-            uint256 requestId = COORDINATOR.requestRandomWords(
-                keyHash,
-                subscriptionId,
-                requestConfirmations,
-                callbackGasLimit,
-                numWords
-            );
-            emit RandomNumberRequested(requestId);
+            requestRandomWords(false);
         }
     }
 
-    function fulfillRandomWords(uint256 /* requestId */, uint256[] memory randomWords) internal override {
-        uint256 randomResult = randomWords[0];
-        emit RandomNumberReceived(randomWords[0]);
+    function getRequestStatus(
+        uint256 _requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
+    }
+
+    // pick random winner once threshold is met for the current vote cycle
+    function fulfillRandomWords(uint256 _requestId, uint256[] calldata _randomWords) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        emit RequestFulfilled(_requestId, _randomWords);
+        uint256 randomResult = _randomWords[0];
         uint randomIndex = randomResult % cycleVoters[currentCycle].length;
         address payable luckyVoter = payable(cycleVoters[currentCycle][randomIndex]);
         uint256 amountToSend = address(this).balance;
-        emit WinnerAnnounced(luckyVoter, amountToSend);
+        emit WinnerAnnounced(luckyVoter, amountToSend, currentCycle);
         luckyVoter.transfer(amountToSend);
         currentCycle++;
-    }
-
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "New owner cannot be the zero address");
-        owner = newOwner;
-    }
-
-    function withdraw() public onlyOwner {
-        payable(owner).transfer(address(this).balance);
     }
 }
