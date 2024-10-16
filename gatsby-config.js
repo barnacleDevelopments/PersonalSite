4d49eac1122c670c48aafbe0df4dc226c68d0b7f
Devin Davis <devin@warriertech.com>
2024-10-16 19:40:44 -0400

    update csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,10 +99,10 @@ module.exports = {
         directives: {
           "script-src": `'self' 'unsafe-inline' data: www.google-analytics.com`,
           "style-src": `'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com`,
-          "img-src": `'self' data: www.google-analytics.com https://arweave.net`,
+          "img-src": `'self' data: www.google-analytics.com https://arweave.net *.arweave.net`,
           "font-src": `'self' data: fonts.gstatic.com`,
           "connect-src":
-            "'self' wss://eth-sepolia.g.alchemy.com https://arweave.net",
+            "'self' wss://eth-sepolia.g.alchemy.com https://arweave.net *.arweave.net",
         },
       },
     },
