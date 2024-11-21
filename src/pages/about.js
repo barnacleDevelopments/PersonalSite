e8e6fae71b7d5194ab8ba60687f9128548cda3b8
Devin Davis <devin@warriertech.com>
2024-11-21 01:56:36 -0400

    update layout of about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@
 import { Link, Paragraph, jsx } from "theme-ui";
 
 // COMPONENTS
-import { Box, Heading } from "theme-ui";
+import { Box, Heading, Grid } from "theme-ui";
 import Seo from "../components/app/Seo";
 import { StaticImage } from "gatsby-plugin-image";
 import CallToAction from "../components/CallToAction";
@@ -21,35 +21,39 @@ const AboutPage = () => {
         <Heading as="h1" variant="hero">
           A Little More About Me
         </Heading>
-        <Box
-          sx={{
-            overflow: "hidden",
-            borderRadius: "10px",
-            mb: 3,
-          }}
-        >
-          <StaticImage
-            style={{ height: "500px" }}
-            src="../images/dev-on-tree.jpeg"
-            alt="dev portrait"
-          />
-        </Box>
-        <Paragraph variant="regular">
-          The thing that separates the software field from others is the
-          incredible people I have met and the amazing teams they have built.
-          Our field encompasses many disciplines, and it is inspiring how we can
-          successfully co-operate to achieve extraordinary outcomes. I have a
-          passion for helping businesses realize their software solutions using
-          the latest web technologies. The challenge of technology and its
-          potential to enable new possibilities draws me into this
-          ever-expanding field. Whether building a solution, gaining deep
-          knowledge on a subject, or increasing my proficiency, I am constantly
-          learning. If you couldn't tell, I like to climb just about anything I
-          can get my hands on. Naturally, if you don't see me at the office, I'm
-          probably at the local bouldering gym. Hearing people talk about their
-          passions fires me up. Please <Link href="/contact">reach out</Link>;
-          I'm always up for a chat!
-        </Paragraph>
+        <Grid gap={3} columns={["1fr", "1fr 1fr", "1.5fr 2fr"]}>
+          <Box
+            sx={{
+              borderRadius: "10px",
+              overflow: "hidden",
+            }}
+          >
+            <StaticImage
+              style={{ height: "500px" }}
+              src="../images/devin_portrait_2.jpg"
+              alt="dev portrait"
+            />
+          </Box>
+          <Box>
+            <Paragraph variant="regular">
+              The thing that separates the software field from others is the
+              incredible people I have met and the amazing teams they have
+              built. Our field encompasses many disciplines, and it is inspiring
+              how we can successfully co-operate to achieve extraordinary
+              outcomes. I have a passion for helping businesses realize their
+              software solutions using the latest web technologies. The
+              challenge of technology and its potential to enable new
+              possibilities draws me into this ever-expanding field. Whether
+              building a solution, gaining deep knowledge on a subject, or
+              increasing my proficiency, I am constantly learning. If you
+              couldn't tell, I like to climb just about anything I can get my
+              hands on. Naturally, if you don't see me at the office, I'm
+              probably at the local bouldering gym. Hearing people talk about
+              their passions fires me up. Please{" "}
+              <Link href="/contact">reach out</Link>; I'm always up for a chat!
+            </Paragraph>
+          </Box>
+        </Grid>
         <CallToAction
           title={"Checkout Some of my Projects"}
           content={"Every project I take on take ownership of."}
