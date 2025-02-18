b87bde784ddd3818cbbe7799230e9f29b1026cfe
devin <devin@mailfence.com>
2025-02-18 10:12:43 -0500

    update the about page description

--- src/pages/about.js
+++ src/pages/about.js
@@ -36,20 +36,20 @@ const AboutPage = () => {
           </Box>
           <Box>
             <Paragraph variant="regular">
-              The thing that separates the software field from others is the
-              incredible people I have met and the amazing teams they have
-              built. Our field encompasses many disciplines, and it is inspiring
-              how we can successfully co-operate to achieve extraordinary
-              outcomes. I have a passion for helping businesses realize their
-              software solutions using the latest web technologies. The
-              challenge of technology and its potential to enable new
-              possibilities draws me into this ever-expanding field. Whether
-              building a solution, gaining deep knowledge on a subject, or
-              increasing my proficiency, I am constantly learning. If you
-              couldn't tell, I like to climb just about anything I can get my
-              hands on. Naturally, if you don't see me at the office, I'm
-              probably at the local bouldering gym. Hearing people talk about
-              their passions fires me up. Please{" "}
+              The software field stands out due to the incredible people I've
+              met and the amazing teams we've built. Our field encompasses many
+              disciplines, and it's inspiring how we co-operate to achieve
+              extraordinary outcomes. I thrive in collaborative environments,
+              embracing teamwork to help businesses realize their software
+              solutions using the latest web technologies. The challenge of
+              technology and its potential to enable new possibilities draws me
+              into this ever-expanding field. Whether building a solution,
+              gaining deep knowledge on a subject, or increasing my proficiency,
+              I am constantly learning. If you couldn't tell, I like to climb
+              just about anything I can get my hands on. Naturally, if you don't
+              see me at the office, I'm probably at the local bouldering gym.
+              Hearing people talk about their passions fires me up. Please reach
+              out; I'm always up for a chat!
               <Link href="/contact">reach out</Link>; I'm always up for a chat!
             </Paragraph>
           </Box>
