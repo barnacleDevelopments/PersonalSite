7574b9b84b4f4f5d9ada2788790bae646e78dfb0
devin <devin@mailfence.com>
2025-03-02 15:06:08 -0500

    cleanup unused packages

--- gatsby-node.js
+++ gatsby-node.js
@@ -187,8 +187,5 @@ exports.onCreateNode = async ({
 
 exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
   const config = getConfig();
-  if (config.externals && config.externals[0]) {
-    config.externals[0]["node:crypto"] = require.resolve("crypto-browserify");
-  }
   actions.replaceWebpackConfig(config);
 };
