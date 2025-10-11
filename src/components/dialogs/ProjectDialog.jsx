92c9e31216c69070914e6505b931fe9b84d92f6a
devin <devin@mailfence.com>
2025-10-11 10:29:04 -0300

    add missing diloag

---
+++ src/components/dialogs/ProjectDialog.jsx
@@ -0,0 +1,65 @@
+/** @jsx jsx */
+import { forwardRef } from "react";
+import { Box, Flex, Heading, jsx } from "theme-ui";
+import { Link } from "gatsby";
+import { CloseButton } from "../app/CloseButton";
+
+const ProjectDialog = forwardRef(({ title, projects }, ref) => {
+  return (
+    <dialog style={{ position: "fixed", zIndex: 10000, padding: 0 }} ref={ref}>
+      {title && projects && (
+        <Box
+          sx={{
+            borderColor: "white",
+            borderRadius: 4,
+            backgroundColor: "white",
+            p: 4,
+          }}
+        >
+          <Box>
+            <Flex
+              sx={{
+                alignItems: "center",
+                justifyContent: "space-between",
+                gap: 3,
+                mb: 3,
+              }}
+            >
+              <Heading sx={{ mb: 0 }} variant="subheading2">
+                {title} - Projects
+              </Heading>
+              <CloseButton
+                sx={{ position: "absolute" }}
+                onClick={() => {
+                  ref.current.close();
+                }}
+              />
+            </Flex>
+            <ul>
+              {projects.map((project) => {
+                return (
+                  <li key={project.title} sx={{ mb: 2 }}>
+                    <Link
+                      to={project.slug}
+                      sx={{
+                        color: "orange",
+                        textDecoration: "none",
+                        "&:hover": {
+                          textDecoration: "underline",
+                        },
+                      }}
+                    >
+                      {project.title}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </Box>
+        </Box>
+      )}
+    </dialog>
+  );
+});
+
+export default ProjectDialog;
