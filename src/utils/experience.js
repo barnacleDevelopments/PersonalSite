a5e266bb22556dec67c01cfe24710b5a2db0b25a
devin <devin@mailfence.com>
2026-03-26 20:44:25 -0300

    round up number of years

--- src/utils/experience.js
+++ src/utils/experience.js
@@ -2,8 +2,8 @@ const CAREER_START_DATE = new Date(2021, 7, 1); // August 2021
 
 export function getYearsOfExperience() {
   const now = new Date();
-  const years = Math.floor(
-    (now - CAREER_START_DATE) / (365.25 * 24 * 60 * 60 * 1000),
+  const years = Math.round(
+    Number.parseFloat(now - CAREER_START_DATE) / (365.25 * 24 * 60 * 60 * 1000),
   );
   return years;
 }
